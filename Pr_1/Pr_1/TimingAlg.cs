241ee3f731cdadefd0e045f080ac57e997c72d8a
1katya6 <98809518+1katya6@users.noreply.github.com>
2022-12-19 21:14:14 +0500

    Add files via upload

---
+++ Pr_1/Pr_1/TimingAlg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pr_1
+{
+    internal class TimingAlg
+    {
+        internal void StartTime() => throw new NotImplementedException();
+
+        internal void StopTime()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object Result()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
